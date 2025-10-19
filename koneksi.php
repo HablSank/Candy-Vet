86cad19ed47fe15005a8c89efa9e4714475c46b8
HablSank <hablsankrumipradhab@gmail.com>
2025-10-19 20:29:54 +0700

    Menambahkan .env.example Untuk Contoh Pengisian .env

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,11 @@
 <?php
-$host = "localhost";
-$user = '***REMOVED***';
-$pass = '';
-$db = '***REMOVED***';
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); 
+$dotenv->load();
+$host = $_ENV['DB_HOST']; 
+$user = $_ENV['DB_USERNAME'];
+$pass = $_ENV['DB_PASSWORD'];
+$db   = $_ENV['DB_DATABASE'];
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
