5c8a77fbbab19631ba78a5e114e26b4c29105894
HablSank <hablsankrumipradhab@gmail.com>
2025-10-22 10:14:52 +0700

    SweetAlert login.php Done

--- login.php
+++ login.php
@@ -14,6 +14,8 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </head>
     <body class="bg-[#FEF3E2] min-h-screen flex items-center justify-center p-4" style="font-family: 'Poppins', sans-serif;">
         <?php 
@@ -29,9 +31,26 @@
             if(mysqli_num_rows($result) > 0){
                 $data = mysqli_fetch_array($result);
                 $_SESSION['user'] = $data;
-                echo '<script>alert("Selamat datang, '.$data['nama'].'"); location.href="admin.php"</script>';
+                echo '<script>
+                    Swal.fire({
+                        icon: "success",
+                        title: "Selamat Datang!",
+                        text: "Halo, '.$data['nama'].'",
+                        showConfirmButton: false,
+                        timer: 1500
+                        }).then(() => {
+                        location.href = "admin.php";
+                        });
+                       </script>';
             }else{
-                echo '<script>alert("Username/password tidak sesuai");</script>';
+            echo '<script>
+                Swal.fire({
+                    icon: "error",
+                    title: "Login Gagal",
+                    text: "Username atau password tidak sesuai",
+                    confirmButtonColor: "#F4631E"
+                });
+            </script>';
             }
         }
         
